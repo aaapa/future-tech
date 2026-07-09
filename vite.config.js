75bc99adeb31c3ec70e885a06cdd14248214e6df
RamilShiyapov <ramil26102006@gmail.com>
2026-07-09 15:09:12 +0300

    Update vite config with custom build output

--- vite.config.js
+++ vite.config.js
@@ -6,14 +6,29 @@ export default defineConfig({
   base: '/future-tech/',
   plugins: [react()],
   resolve: {
-    alias: {
-      '@': resolve(__dirname, 'src'),
+    alias: [{ find: '@/', replacement: resolve('src') + '/' }],
+  },
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, 'index.html'),
+      },
+      output: {
+        entryFileNames: 'main.js',
+        assetFileNames: (assetInfo) => {
+          if (assetInfo.name === 'index.css')
+            return 'main.css';
+          return '[name].[ext]';
+        },
+      },
     },
+    assetsDir: '',
+    outDir: 'dist',
   },
   css: {
     preprocessorOptions: {
       scss: {
-        additionalData: '@use "@/app/styles/helpers" as *;\n',
+        additionalData: `@use '@/app/styles/helpers' as *;\n`,
         silenceDeprecations: ['legacy-js-api'],
       },
     },
